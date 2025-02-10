5ca93f0903a6510b88e2b58615a3f25938edd76f
Bikman Ntiruhungwa <bikmangeek@gmail.com>
2025-02-10 08:33:47 +0200

    runtime.txt added_X

--- app.py
+++ app.py
@@ -11,5 +11,8 @@ app.register_blueprint(statistics_bp)
 
 app.register_blueprint(dataset_bp)
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = os.environ.get('PORT', 8080)
+    app.run(debug=True, host='0.0.0.0', port=port)
+
