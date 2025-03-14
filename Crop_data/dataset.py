8ca1ac7ddde8b96b0c7c2254c59e7521a360cf4e
Bikman Ntiruhungwa <bikmangeek@gmail.com>
2025-03-14 07:55:53 +0200

    some comment removed

--- Crop_data/dataset.py
+++ Crop_data/dataset.py
@@ -3,13 +3,3 @@ import pandas as pd
 def dataset():
     return pd.read_csv('Crop_data/dataset/Crop_recommendation.csv')
     # return pd.read_csv('Crop_data/dataset/Plant_Parameters.csv')
-
-# import pandas as pd
-
-# class Dataset:
-#     def __init__(self):
-
-#         self.data = pd.read_csv("Crop_data/dataset/Crop_recommendation.csv")
-
-#     def get_data(self):
-#         return self.data
