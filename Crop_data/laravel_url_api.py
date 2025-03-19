2d0ba5a7c9f4330886949b0b15bab6098b26493f
Bikman Ntiruhungwa <bikmangeek@gmail.com>
2025-03-19 22:33:40 +0200

    url added

--- Crop_data/laravel_url_api.py
+++ Crop_data/laravel_url_api.py
@@ -1,1 +1,2 @@
-Laravel_url_api = "http://127.0.0.1:8000/api/crop-getArduinoData"
+# Laravel_url_api = "http://127.0.0.1:8000/api/crop-getArduinoData"
+Laravel_url_api = "https://smart-soil-laravel-api.up.railway.app/api/crop-getArduinoData"
